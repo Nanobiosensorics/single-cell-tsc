b1d949ae0e5c4fb6abe989155b50f97a6c88e91f
Bálint Béres <balintberes96@gmail.com>
2023-07-25 08:22:58 +0200

    changes

--- scripts/train-run.sh
+++ scripts/train-run.sh
@@ -7,29 +7,18 @@ declare -a arr=("hela-mdamb231-mcf7-lclc" "mdamb231-mcf7" "mdamb231-mcf7-lclc" "
 
 for i in "${arr[@]}"
 do
-
     echo -p $SRC_PATH/0.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/0.5hrs/$i/
     python3 main.py -p $SRC_PATH/0.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/0.5hrs/$i/ -m all -i 1 -v cross_val
-    # echo -p $SRC_PATH/0.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/0.5hrs/$i/ -c $SRC_PATH/0.5hrs/
-    # python3 eval.py -p $SRC_PATH/0.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/0.5hrs/$i/ -c $SRC_PATH/0.5hrs/
 
     echo -p $SRC_PATH/1hrs/$i/sampled/train-test/ -d $RESULT_PATH/1hrs/$i/
     python3 main.py -p $SRC_PATH/1hrs/$i/sampled/train-test/ -d $RESULT_PATH/1hrs/$i/ -m all -i 1 -v cross_val
-    # echo -p $SRC_PATH/1hrs/$i/sampled/train-test/ -d $RESULT_PATH/1hrs/$i/ -c $SRC_PATH/1hrs/
-    # python3 eval.py -p $SRC_PATH/1hrs/$i/sampled/train-test/ -d $RESULT_PATH/1hrs/$i/ -c $SRC_PATH/1hrs/
 
     echo -p $SRC_PATH/1.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/1.5hrs/$i/
     python3 main.py -p $SRC_PATH/1.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/1.5hrs/$i/ -m all -i 1 -v cross_val
-    # echo -p $SRC_PATH/1.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/1.5hrs/$i/ -c $SRC_PATH/1.5hrs/
-    # python3 eval.py -p $SRC_PATH/1.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/1.5hrs/$i/ -c $SRC_PATH/1.5hrs/
 
     echo -p $SRC_PATH/2hrs/$i/sampled/train-test/ -d $RESULT_PATH/2hrs/$i/
     python3 main.py -p $SRC_PATH/2hrs/$i/sampled/train-test/ -d $RESULT_PATH/2hrs/$i/ -m all -i 1 -v cross_val
-    # echo -p $SRC_PATH/2hrs/$i/sampled/train-test/ -d $RESULT_PATH/2hrs/$i/ -c $SRC_PATH/2hrs/
-    # python3 eval.py -p $SRC_PATH/2hrs/$i/sampled/train-test/ -d $RESULT_PATH/2hrs/$i/ -c $SRC_PATH/2hrs/
 
     echo -p $SRC_PATH/2.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/2.5hrs/$i/
     python3 main.py -p $SRC_PATH/2.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/2.5hrs/$i/ -m all -i 1 -v cross_val
-    # echo -p $SRC_PATH/2.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/2.5hrs/$i/ -c $SRC_PATH/2.5hrs/
-    # python3 eval.py -p $SRC_PATH/2.5hrs/$i/sampled/train-test/ -d $RESULT_PATH/2.5hrs/$i/ -c $SRC_PATH/2.5hrs/
 done
