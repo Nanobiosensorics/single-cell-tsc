8b14faf47a5a6caae09c20b956409405581c91ca
Bálint Béres <balintberes96@gmail.com>
2023-09-17 10:14:40 +0200

    scripts updated

--- scripts/train-run.sh
+++ scripts/train-run.sh
@@ -3,7 +3,7 @@
 SRC_PATH=$1
 RESULT_PATH=$2
 
-declare -a cell_types=("hela-lclc-mcf7-mdamb231" "mcf7-mdamb231" "lclc-mcf7-mdamb231" "hela-lclc-mcf7-mdamb231-preo" "breastcancer-hela-lclc" "breastcancer-lclc" "breastcancer-hela-lclc-preo")
+declare -a cell_types=("hela-lclc-mcf7-mdamb231" "hela-lclc-mcf7-mdamb231-preo" "breastcancer-hela-lclc"  "breastcancer-hela-lclc-preo" "breastcancer-hela" "hela-mcf7-mdamb231" "mcf7-mdamb231")
 declare -a times=(30 60 90 120 150)
 for i in "${cell_types[@]}"
 do
